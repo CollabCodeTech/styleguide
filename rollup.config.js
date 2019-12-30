607ff12563686d5d7166ef5073602531382927e9
Marco Bruno <marco.bruno.br@gmail.com>
2019-12-30 16:43:44 +0000

    :wrench: Change plugin eslint to svelte3

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable global-require */
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-unused-vars */
 import styles from './styles/styles.svelte';
 
 import Logo from './components/Logo.svelte';
 
-// eslint-disable-next-line import/prefer-default-export
 export { Logo };
